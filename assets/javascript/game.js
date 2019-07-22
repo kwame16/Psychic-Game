80ed7d7ecd3073dac2c78ca25b32686690349c8d
kwame16 <51427903+kwame16@users.noreply.github.com>
2019-07-21 23:24:49 -0400

    added more letters to the game for more difficulty...The whole alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 // Creates an array that lists out all of the options.
-var computerChoices = ['a', 'b', 'c'
+var computerChoices = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
-// 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-// 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+
 
 var wins = 0;
 var losses = 0;
